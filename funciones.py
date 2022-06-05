079239498ba008d7c9608861ad4beaaac140e811
Dana Aibar <dana.aibar@mi.unc.ar>
2022-06-05 15:02:54 -0300

    Funciones suma.

--- funciones.py
+++ funciones.py
@@ -1,14 +1,35 @@
 #crearemos nuestras propias funciones
+#Area funciones
 def mensaje():#declaracion de la funcion
     #cuerpo de la funcion.
     print("Estamos aprendiendo Python.")
     print("Estamos aprendiendo instrucciones basicas")
     print("Poco a poco iremos avanzando")
-#fin funcion
 
-mensaje() #llamada a la funcion.
-mensaje()
-mensaje()
-print("Ejecutando codigo fuera de la funcion")
+def suma(): 
+    num1 = 5
+    num2 = 7
+    print(num1 + num2)
+
+def suma_p():
+    num1 = int(input("Ingrese un numero: "))
+    num2 = int(input("Ingrese otro numero: "))
+    print(num1 + num2)
+
+def suma_pe(num1, num2):
+    print(num1 + num2)
+
+def sumar(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+#fin area funciones
+
+#Area principal
+
+suma()
+suma_pe(5, 7)
+suma_p()
+print (sumar(5, 7))
 
 
