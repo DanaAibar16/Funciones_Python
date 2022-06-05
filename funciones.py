9bd331182b80799a4129c1209eac7e55d1ff857e
Dana Aibar <dana.aibar@mi.unc.ar>
2022-06-05 14:33:47 -0300

    Primera funcion.

---
+++ funciones.py
@@ -0,0 +1,10 @@
+#crearemos nuestras propias funciones
+def mensaje():#declaracion de la funcion
+    #cuerpo de la funcion.
+    print("Estamos aprendiendo Python.")
+    print("Estamos aprendiendo instrucciones basicas")
+    print("Poco a poco iremos avanzando")
+#fin funcion
+
+mensaje() #llamada a la funcion.
+
